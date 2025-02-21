4765fc6020cafb8f58692a74efb69e5137551e4e
Aayush259 <aayushk4980@gmail.com>
2025-02-22 00:23:54 +0530

    Created mortgage calculator

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,11 @@
+import Calculator from "@/components/mortgage/Calculator";
+import "@/styles/mortgage.css";
+
+export default function Page() {
+
+    return (
+        <main>
+            <Calculator />
+        </main>
+    );
+};
--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -19,3 +19,14 @@ export interface IGotQuestionCategory {
     categoryName: string;
     questions: IGotQuestions[];
 }
+
+export interface ICalculatorInput {
+    homePrice: number;       // In dollars
+    downPayment: number;     // In dollars
+    interestRate: number;    // Annual interest rate in percentage (e.g., 4.5)
+    loanTerm: number;        // In years (e.g., 30)
+    propertyTaxes: number;   // Monthly property taxes in dollars
+    homeInsurance: number;   // Monthly homeowners insurance in dollars
+    hoaFees: number;         // Monthly HOA fees in dollars
+    utilities: number;       // Monthly utilities cost in dollars
+}
