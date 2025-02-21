ff4978567b38988dd18012112a11181eb490234f
Aayush259 <aayushk4980@gmail.com>
2025-02-21 18:50:45 +0530

    About page init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import OurMission from "@/components/about/OurMission";
+import "@/styles/about.css";
+
+export default function Page() {
+
+    return (
+        <main>
+            <OurMission />
+        </main>
+    );
+};
